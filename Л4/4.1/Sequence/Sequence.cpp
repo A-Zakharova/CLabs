7449246a88f99844b903e5a6ba2886047e314c33
A-Zakharova <65176941+A-Zakharova@users.noreply.github.com>
2020-06-07 22:00:41 +0300

    Add files via upload

---
+++ Л4/4.1/Sequence/Sequence.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <string>
+
+
+int main()
+{
+    char line[] = "abdabdabdabdabd";
+    char subsequence[] = "abd";
+
+    char* pL = line;
+    char* pS = subsequence;
+
+    int flagOk = 0;
+    int counter = 0;
+
+    while (*pL != 0) {
+        while ((*pS != 0)) {
+            if (*pL == *pS) {
+                flagOk = 1;
+                pS++;
+            }
+            else {
+                flagOk = 0;
+                break;
+
+            }
+            pL++;
+        }
+        if (flagOk) {
+            counter++;
+            pS = subsequence;
+        }
+        else {
+            break;
+        }
+    }
+
+    if ((counter > 1) && (flagOk))
+        printf("Your line is periodic sequence");
+    else
+        printf("Your line is not periodic sequence");
+
+}
